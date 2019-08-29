55446f721549d38a5efbdf82052b2c76871317a2
K G Pravin Kumar <35860045+pravinkumarkg@users.noreply.github.com>
2019-08-29 22:00:35 +0530

    Create gross_salary.cpp

---
+++ gross_salary.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float basic,gross,da,hra;
+	cin>>basic;
+	if(basic<=10000)
+	{
+		da=basic*0.8;
+		hra=basic*0.2;
+	}
+	else if(basic<=20000)
+	{
+		da=basic*0.9;
+		hra=basic*0.25;
+	}
+	else 
+	{
+		da=basic*0.95;
+		hra=basic*0.3;
+	}
+	gross=basic+hra+da;
+	cout<<gross;
+	return 0;
+}
